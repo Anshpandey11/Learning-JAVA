4454bbf5b279be411cb66c0e983d63051fd11a06
anshpandey11200630@gmail.com <anshpandey11200630@gmail.com>
2026-09-16 23:46:37 +0530

    Learning

---
+++ Practice before DSA/Arrays/Appears_only_once.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Appears_only_once {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        int r = 0;
+        for (int i = 0; i < n; i++) {
+            int c = 0;
+            for (int j = 0; j < n; j++) {
+                if (a[i] == a[j]) {
+                    c++;
+                }
+            }
+            if (c == 1) {
+                r = a[i];
+            }
+        }
+        System.out.println(r);
+    }
+}
