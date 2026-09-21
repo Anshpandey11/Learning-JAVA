1cf79ca1a2d6534118c605f45d7365056ca86b34
anshpandey11200630@gmail.com <anshpandey11200630@gmail.com>
2026-09-21 23:39:55 +0530

    Learning

--- Practice before DSA/Arrays/Sum_element.java
+++ Practice before DSA/Arrays/Sum_element.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class Sum_element {
     public static void main(String[] args) {
-          Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int a[] = new int[n];
         for (int i = 0; i < n; i++) {
             a[i] = sc.nextInt();
         }
         int sum = 0;
-        for(int i=0;i<n;i++){
+
+        for (int i = 0; i < n; i++) {
             sum = sum + a[i];
         }
         System.out.println(sum);
