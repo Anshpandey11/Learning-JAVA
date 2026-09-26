f34e8ae4b0f55c4de48c18ce4d9e052593ec5ab6
anshpandey11200630@gmail.com <anshpandey11200630@gmail.com>
2026-09-26 23:20:39 +0530

    Learning

--- Practice before DSA/String/String_compression.java
+++ Practice before DSA/String/String_compression.java
@@ -4,6 +4,19 @@ public class String_compression {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        
+        String newstr = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            Integer c = 1;
+            while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                c++;
+                i++;
+            }
+            newstr += str.charAt(i);
+            if (c > 1) {
+                newstr += c.toString();
+            }
+        }
+        System.out.println(newstr);
     }
 }
